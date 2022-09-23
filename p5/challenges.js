aaa3bbaaca71a95c2438839976bcae12d2c4c1a8
Linnea B <linnea.k.bergstrom@gmail.com>
2022-09-23 14:17:41 +0200

    mi primer commit

---
+++ p5/challenges.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Probando P5</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js"></script>
+</head>
+<body>
+<script>
+
+function setup() {
+  createCanvas(200, 200);
+}
+// three-digit hexadecimal RGB notation
+function draw() {
+
+//RGB Integer Values
+  background(255, 204, 0);
+  rectMode(CENTER);
+  fill(25, 200, 10);
+  rect(100, 100, 100, 100);
+
+} 
+    </script>
+</body>
+
+</html>
+
